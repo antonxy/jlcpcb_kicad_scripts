2de28ed0b480cdf20a6ca541cd645b460ebdf676
antonxy <anton.schirg@posteo.de>
2021-07-08 21:14:35 +0200

    Add files via upload

---
+++ jlcpcb_bom.py
@@ -0,0 +1,59 @@
+#
+# Example python script to generate a BOM from a KiCad generic netlist
+#
+# Example: Sorted and Grouped CSV BOM
+#
+
+"""
+    @package
+    BOM export matching JLCPCB format.
+    Each component should have a field named LCSC containing the LCSC part nr.
+    
+    Command line:
+    python "pathToFile/jlcpcb_bom.py" "%I" "%O.csv"
+    
+    Place in C:\Program Files\KiCad\bin\scripting\plugins\ and access in eeschema through Tools > Generate Bill of Materials > Add new plugin
+"""
+
+# Import the KiCad python helper module and the csv formatter
+import kicad_netlist_reader
+import csv
+import sys
+
+# Generate an instance of a generic netlist, and load the netlist tree from
+# the command line option. If the file doesn't exist, execution will stop
+net = kicad_netlist_reader.netlist(sys.argv[1])
+
+# Open a file to write to, if the file cannot be opened output to stdout
+# instead
+try:
+    f = open(sys.argv[2], 'w')
+except IOError:
+    e = "Can't open output file for writing: " + sys.argv[2]
+    print(__file__, ":", e, sys.stderr)
+    f = sys.stdout
+
+# Create a new csv writer object to use as the output formatter
+out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
+
+# Output a set of rows for a header providing general information
+out.writerow(['Comment', 'Designator', 'Footprint', 'LCSC Part #'])
+
+# Get all of the components in groups of matching parts + values
+# (see ky_generic_netlist_reader.py)
+grouped = net.groupComponents()
+
+# Output all of the component information
+for group in grouped:
+    refs = ""
+
+    # Add the reference of every component in the group and keep a reference
+    # to the component so that the other data can be filled in once per group
+    for component in group:
+        refs += component.getRef() + ","
+        c = component
+
+    # Fill in the component groups common data
+    out.writerow([c.getValue(), refs, c.getFootprint(), c.getField("LCSC")])
+
+
---
+++ jlcpcb_cpl.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pcbnew
+import csv
+
+"""
+pcbnew action plugin to export component placement list according to JLCPCB format.
+SMT and Through-Hole will be contained in the file.
+
+
+Windows 7/Windows 10:
+Install the plugins in: %APPDATA%\kicad\scripting\plugins
+
+GNU/Linux:
+Install the plugins in: ~/.kicad/scripting/plugins or ~/.kicad_plugins
+
+Access from pcbnew through Tools > External Plugins
+"""
+
+class jlcpcb_cpl(pcbnew.ActionPlugin):
+    def defaults(self):
+        """
+        Method defaults must be redefined
+        self.name should be the menu label to use
+        self.category should be the category (not yet used)
+        self.description should be a comprehensive description
+          of the plugin
+        """
+        self.name = "Export CPL in JLC Format"
+        self.category = "Export"
+        self.description = "Generate CPL for JLCPCB assembly"
+
+    def Run(self):
+            my_board = pcbnew.GetBoard()
+            outname = os.path.join(os.path.dirname(my_board.GetFileName()), "jlc_cpl.csv")
+                
+            f = open(outname, 'w')
+            out = csv.writer(f, lineterminator='\n', delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
+            out.writerow(["Designator", "Mid X", "Mid Y", "Layer", "Rotation"])
+
+            for module in my_board.GetModules():
+                if module.GetAttributes() != pcbnew.MOD_VIRTUAL:
+                    out.writerow([
+                        module.GetReference(),
+                        "{}mm".format(pcbnew.ToMM(module.GetPosition().x)),
+                        "{}mm".format(pcbnew.ToMM(module.GetPosition().y)),
+                        "Top" if module.GetLayer() == 0 else "Bottom",
+                        module.GetOrientation()/10])
+
+
+jlcpcb_cpl().register()
